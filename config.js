87e75634eacdeb55e6bf5be52fb8df8da7128d31
Arnab Mac <arnab.kundu1990@gmail.com>
2024-03-03 21:26:05 +0530

    Issue fixed

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export default {
-  API_KEY: 'YourOpenWeatherKeyHere',
-  GOOGLE_KEY: 'YourGoogleKeyHere',
+  API_KEY: '849338767c0e95025b5559533d26b7c4',
+  GOOGLE_KEY: 'AIzaSyDo_TQvYSKYtHr6Aqflf6Y0pmduoiM7ARc',
 };
